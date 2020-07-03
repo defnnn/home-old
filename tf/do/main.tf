0a754a6839e9758291d094014d89509026ad30d1
Amanibhavam <iam@defn.sh>
2020-07-03 14:21:59 -0700

    use for_each to DRY

--- tf/do/main.tf
+++ tf/do/main.tf
@@ -13,21 +13,15 @@ data "digitalocean_vpc" "sfo3" {
   name = "default-sfo3"
 }
 
-data "digitalocean_droplet_snapshot" "defn_home_nyc1" {
-  name_regex  = "^defn-home"
-  region      = "nyc1"
-  most_recent = true
+locals {
+  regions = ["nyc1", "sfo2", "sfo3"]
 }
 
-data "digitalocean_droplet_snapshot" "defn_home_sfo2" {
-  name_regex  = "^defn-home"
-  region      = "sfo2"
-  most_recent = true
-}
+data "digitalocean_droplet_snapshot" "defn_home" {
+  for_each = toset(local.regions)
 
-data "digitalocean_droplet_snapshot" "defn_home_sfo3" {
   name_regex  = "^defn-home"
-  region      = "sfo3"
+  region      = each.key
   most_recent = true
 }
 
@@ -38,9 +32,7 @@ resource "digitalocean_project" "defn" {
 resource "digitalocean_project_resources" "defn" {
   project = digitalocean_project.defn.id
   resources = [
-    digitalocean_droplet.defn_nyc1.urn,
-    digitalocean_droplet.defn_sfo2.urn,
-    digitalocean_droplet.defn_sfo3.urn
+    for e in digitalocean_droplet.defn : e.urn
   ]
 }
 
@@ -48,9 +40,7 @@ resource "digitalocean_firewall" "defn" {
   name = "default"
 
   droplet_ids = [
-    digitalocean_droplet.defn_nyc1.id,
-    digitalocean_droplet.defn_sfo2.id,
-    digitalocean_droplet.defn_sfo3.id
+    for e in digitalocean_droplet.defn : e.id
   ]
 
   inbound_rule {
@@ -94,70 +84,28 @@ resource "digitalocean_firewall" "defn" {
   }
 }
 
-resource "digitalocean_volume" "defn_nyc1" {
-  region                  = "nyc1"
-  name                    = "volume-nyc1-01"
+resource "digitalocean_volume" "defn" {
+  for_each = toset(local.regions)
+
+  region                  = each.key
+  name                    = "volume-${each.key}-01"
   size                    = 10
   initial_filesystem_type = "ext4"
 }
 
-resource "digitalocean_volume_attachment" "defn_nyc1" {
-  droplet_id = digitalocean_droplet.defn_nyc1.id
-  volume_id  = digitalocean_volume.defn_nyc1.id
+resource "digitalocean_volume_attachment" "defn" {
+  for_each = toset(local.regions)
+
+  droplet_id = digitalocean_droplet.defn[each.key].id
+  volume_id  = digitalocean_volume.defn[each.key].id
 }
 
-resource "digitalocean_volume" "defn_sfo2" {
-  region                  = "sfo2"
-  name                    = "volume-sfo2-01"
-  size                    = 10
-  initial_filesystem_type = "ext4"
-}
+resource "digitalocean_droplet" "defn" {
+  for_each = toset(local.regions)
 
-resource "digitalocean_volume_attachment" "defn_sfo2" {
-  droplet_id = digitalocean_droplet.defn_sfo2.id
-  volume_id  = digitalocean_volume.defn_sfo2.id
-}
-
-resource "digitalocean_volume" "defn_sfo3" {
-  region                  = "sfo3"
-  name                    = "volume-sfo3-01"
-  size                    = 10
-  initial_filesystem_type = "ext4"
-}
-
-resource "digitalocean_volume_attachment" "defn_sfo3" {
-  droplet_id = digitalocean_droplet.defn_sfo3.id
-  volume_id  = digitalocean_volume.defn_sfo3.id
-}
-
-resource "digitalocean_droplet" "defn_nyc1" {
-  image  = data.digitalocean_droplet_snapshot.defn_home_nyc1.id
-  name   = "defn-nyc1"
-  region = "nyc1"
-  size   = "s-1vcpu-2gb"
-  ipv6   = true
-
-  lifecycle {
-    ignore_changes = [image]
-  }
-}
-
-resource "digitalocean_droplet" "defn_sfo2" {
-  image  = data.digitalocean_droplet_snapshot.defn_home_sfo2.id
-  name   = "defn-sfo2"
-  region = "sfo2"
-  size   = "s-1vcpu-2gb"
-  ipv6   = true
-
-  lifecycle {
-    ignore_changes = [image]
-  }
-}
-
-resource "digitalocean_droplet" "defn_sfo3" {
-  image  = data.digitalocean_droplet_snapshot.defn_home_sfo3.id
-  name   = "defn-sfo3"
-  region = "sfo3"
+  image  = data.digitalocean_droplet_snapshot.defn_home[each.key].id
+  name   = "defn-${each.key}"
+  region = each.key
   size   = "s-1vcpu-2gb"
   ipv6   = true
 
